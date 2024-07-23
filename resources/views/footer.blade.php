9653987f1374c2188edbe9a249dae12546ab7bab
wahyudedi <wdedyk@gmail.com>
2024-07-23 13:56:17 +0700

    deleted:    public/home/Readme.txt 	deleted:    public/home/assets/scss/Readme.txt 	deleted:    public/home/index.html 	deleted:    public/home/portfolio-details.html 	deleted:    public/home/service-details.html 	deleted:    public/home/starter-page.html 	new file:   resources/views/footer.blade.php 	new file:   resources/views/head.blade.php 	new file:   resources/views/header.blade.php 	new file:   resources/views/home.blade.php 	modified:   resources/views/product-details.blade.php 	new file:   resources/views/scripts.blade.php 	modified:   resources/views/service-details.blade.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/web.php

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="copyright text-center ">
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">Vesperr</strong> <span>All Rights Reserved</span></p>
+      </div>
+      <div class="social-links d-flex justify-content-center">
+        <a href=""><i class="bi bi-twitter-x"></i></a>
+        <a href=""><i class="bi bi-facebook"></i></a>
+        <a href=""><i class="bi bi-instagram"></i></a>
+        <a href=""><i class="bi bi-linkedin"></i></a>
+      </div>
+      <div class="credits">
+        Designed by <a href="https://wahyudedik.my.id/">BootstrapMade</a>
+      </div>
+    </div>
+
+  </footer>
---
+++ resources/views/scripts.blade.php
@@ -0,0 +1,11 @@
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('home/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('home/assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{ asset('home/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{ asset('home/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+  <script src="{{ asset('home/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{ asset('home/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+  <script src="{{ asset('home/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+
+  <!-- Main JS File -->
+  <script src="{{ asset('home/assets/js/main.js')}}"></script>
